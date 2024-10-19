c4bfd2d85eeb5440896cd5454eb5bcf7dbbff877
Jlorduyz <jorgelorduyeli4@outlook.com>
2024-10-18 20:12:47 -0500

    correcion middlework

--- middlewares/bad_request_handler.js
+++ middlewares/bad_request_handler.js
@@ -1,8 +1,12 @@
 const bad_request_handler = (error, req, res, next) => {
-  return res.status(400).json({
-    success: false,
-    message: `La petición no corresponde al modelo manejado en la base de datos: ${error.message}`,
-  });
+  if (error.status === 400) {
+    return res.status(400).json({
+      success: false,
+      message: `La petición no corresponde al modelo manejado en la base de datos: ${error.message}`,
+    });
+  }
+
+  next(error);
 };
 
 export default bad_request_handler;
